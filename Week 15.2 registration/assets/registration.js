d84bca47cc1196f4276889ca875dab00424e5298
LikaSobolenko <91249749+LikaSobolenko@users.noreply.github.com>
2022-02-13 20:32:28 +0700

    Add files via upload

---
+++ Week 15.2 registration/assets/registration.js
@@ -0,0 +1,31 @@
+function check() {
+    let name = document.getElementById("first");
+    let lastname = document.getElementById("last");
+    let email = document.getElementById("email");
+    let tel = document.getElementById("tel");
+    let password = document.getElementById("password");
+
+    document.getElementById('errorMessage').innerHTML = '';
+
+    if (name.value == '') {
+        document.getElementById('errorMessage').innerHTML += 'Ваше Имя не заполнено<br />';
+    }
+
+    if (lastname.value == '') {
+        document.getElementById('errorMessage').innerHTML += 'Ваша Фамилия не заполнена<br />';
+    }
+
+    if (email.value == '') {
+        document.getElementById('errorMessage').innerHTML += 'Ваш email не заполнен<br />';
+    }
+
+    if (tel.value == '') {
+        document.getElementById('errorMessage').innerHTML += 'Ваш Номер телефона не заполнен<br />';
+    }
+
+    if (password.value == '') {
+        document.getElementById('errorMessage').innerHTML += 'Ваш Пароль не заполнен<br />';
+    }
+
+
+}
